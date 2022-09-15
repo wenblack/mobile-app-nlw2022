5e63860252fbd065eba1040d441b437db3511b8f
Wender Barbosa <wenblack012@gmail.com>
2022-09-15 00:58:34 -0300

    Create Loading component and Home Screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
+
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
 import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
-  return (
-      <Background >
+  const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
 
-      </Background>
+
+  return (
+    <Background >
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+    {fontsLoader ? <Home /> : <Loading />}
+    </Background>
   )
 }
 
